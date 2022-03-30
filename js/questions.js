ecad3cbcd72dc579342653a3d216492b86c2dc16
latifasalsab <72429520+latifasalsab@users.noreply.github.com>
2022-03-30 17:10:33 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Untuk menerima masukan berupa pilihan adalah",
+    answer: "Radio",
+    options: [
+      "Submit",
+      "Radio",
+      "Text",
+      "Reset"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Contoh Web browser kecuali...",
+    answer: "Google Drive",
+    options: [
+      "Mozilla Firefox",
+      "Google Chrome",
+      "Internet Explorer",
+      "Google Drive"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Nama unik yang diberikan untuk mengidentifikasi nama server komputer seperti web server atau email server di jaringan komputer ataupun internet adalah",
+    answer: "Domain",
+    options: [
+      "Domain",
+      "Web",
+      "Web",
+      "Hosting"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Bahasa pemrograman web yang proses pengolahannya dilakukan di sisi client dan dilakukan oleh web browser sebagai clientnya adalah",
+    answer: "Client Side Programming",
+    options: [
+      "Client Side Programming",
+      "Web Side Programming",
+      "Server Side Programming",
+      "Host Side Programming"
+    ]
+  },
+    {
+    numb: 5,
+    question: "CSS merupakan singkatan dari",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Cascading Style Sheet",
+      "Cascading Select Style",
+      "Cascading Select Sheet",
+      "Cascading Sheet Style"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
